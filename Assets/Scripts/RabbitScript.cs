a4077d5df84320eb3eb16eda1b20fd76dd7e2dd3
pacokwon <haechank@gmail.com>
2020-07-31 16:59:25 +0900

    add: change direction on grass spot

--- Assets/Scripts/RabbitScript.cs
+++ Assets/Scripts/RabbitScript.cs
@@ -1,35 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RabbitScript : MonoBehaviour {
     private Animator animator;
     private Rigidbody rb;
-    private float wonderTime;
+    private float wanderTime;
+
+    private Vector3 target;
+
+    private enum RabbitState {
+        Dead,
+        Escaping,
+        Targeting,
+        Wandering
+    }
+
+    private RabbitState currentState;
 
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
         animator.speed = 3;
-        wonderTime = Random.Range(1.0f, 2.0f);
+        wanderTime = Random.Range(1.0f, 2.0f);
+        currentState = RabbitState.Wandering;
     }
 
     // Update is called once per frame
     void Update() {
-        if (!animator.GetBool("dead")) {
-            if (wonderTime > 0) {
+        if (currentState == RabbitState.Wandering) {
+            if (wanderTime > 0) {
                 animator.SetTrigger("moving");
-                wonderTime -= Time.deltaTime;
+                wanderTime -= Time.deltaTime;
             } else {
-                wonderTime = Random.Range(1.0f, 2.0f);
+                wanderTime = Random.Range(1.0f, 2.0f);
                 transform.Rotate(0, Random.Range(-120, 120), 0, Space.World);
             }
 
-            Collider[] colliders = Physics.OverlapSphere(transform.position, 10.0f);
-            foreach (var collider in colliders) {
-                Debug.Log("Collision: " + collider.tag);
+            Collider[] grassColliders = Physics.OverlapSphere(transform.position, 10.0f).Where(coll => coll.tag == "Grass").ToArray();
+
+            if (grassColliders.Length > 0) {
+                currentState = RabbitState.Targeting;
+                transform.rotation = Quaternion.LookRotation(grassColliders[0].transform.position - transform.position, Vector3.up);
+                target = grassColliders[0].transform.position;
+                Debug.Log("Targeting");
             }
+        } else if (currentState == RabbitState.Targeting) {
+            Debug.DrawLine(transform.position, target, Color.white);
+            Debug.DrawLine(transform.position, transform.position + transform.forward, Color.white);
+            transform.rotation = Quaternion.LookRotation(target - transform.position, Vector3.up);
+            animator.SetTrigger("moving");
         }
     }
 }
