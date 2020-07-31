ba597b97a8e2cc0d01089e98c2464c8b96dfd64c
pacokwon <haechank@gmail.com>
2020-07-31 20:56:42 +0900

    add: health bar scripts

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public static float maxHealth = 1000f;
+    public float currentHealth = maxHealth;
+    public RectTransform healthbar;
+
+    public bool TakeDamage(float amount) {
+        currentHealth -= amount;
+
+        if (currentHealth <= 0) {
+            currentHealth = 0f;
+        }
+
+        healthbar.sizeDelta = new Vector2(currentHealth * 0.1f, healthbar.sizeDelta.y);
+
+        return currentHealth == 0;
+    }
+}
---
+++ Assets/Scripts/HealthBarCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update() {
+        transform.LookAt(Camera.main.transform); 
+    }
+}
