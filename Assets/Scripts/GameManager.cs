4d42d5348185904ff68f05970a8ce4ac7a05a93c
KyusungSeo <seo3650@kaist.ac.kr>
2020-08-01 14:27:44 +0900

    Add panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -142,6 +142,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /* Pause */
         if (Input.GetKeyDown(KeyCode.P)) {
             if (panel.activeSelf) {
                 Time.timeScale = 1;
