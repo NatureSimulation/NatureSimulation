67de3412d5f3f7ac5dde2d181cc9cbb44d8a229d
KyusungSeo <seo3650@kaist.ac.kr>
2020-08-03 22:47:22 +0900

    Remove comment

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -219,22 +219,22 @@ public abstract class Animal : MonoBehaviour {
     }
 
     protected void TryBreeding() {
-        // if (target.tag != this.tag)
-        //     return;
-        // float distance = (target.transform.position - transform.position).magnitude;
-        // if (target.tag == this.tag && distance < minBreedDistance && leftTimeForBreeding < 0) {
-        //     float x = this.transform.position.x + Random.Range(-20, 20);
-        //     float z = this.transform.position.z + Random.Range(-20, 20);
-        //     float y;
-        //     try {
-        //         y = GameManager.instance.getHeight(x, z);
-        //     } catch (System.Exception) {
-        //         return;
-        //     }
+        if (target.tag != this.tag)
+            return;
+        float distance = (target.transform.position - transform.position).magnitude;
+        if (target.tag == this.tag && distance < minBreedDistance && leftTimeForBreeding < 0) {
+            float x = this.transform.position.x + Random.Range(-20, 20);
+            float z = this.transform.position.z + Random.Range(-20, 20);
+            float y;
+            try {
+                y = GameManager.instance.getHeight(x, z);
+            } catch (System.Exception) {
+                return;
+            }
 
-        //     GameObject child = Instantiate(childPrefab, new Vector3(x, y, z), Quaternion.identity);
-        //     GameManager.instance.breed(child.tag);
-        //     leftTimeForBreeding = coolTimeBreeding;
-        // }
+            GameObject child = Instantiate(childPrefab, new Vector3(x, y, z), Quaternion.identity);
+            GameManager.instance.breed(child.tag);
+            leftTimeForBreeding = coolTimeBreeding;
+        }
     }
 }
