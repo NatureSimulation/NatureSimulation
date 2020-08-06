1e59b639b33ec69e979790aab57eede5d42831ec
KyusungSeo <seo3650@kaist.ac.kr>
2020-08-06 16:22:57 +0900

    Fix status key shortcut

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -429,7 +429,7 @@ public class GameManager : MonoBehaviour
     {
         UpdateAndDisplayTime();
         /* Pause */
-        if (Input.GetKeyDown(KeyCode.P)) {
+        if (Input.GetKeyDown(KeyCode.Q)) {
             panel.SetActive(!panel.activeSelf);
             if (!isHuntMode)
                 animalPanel.SetActive(!animalPanel.activeSelf);
